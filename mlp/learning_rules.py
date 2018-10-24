0933e0fd273e1f19fd2cdecfe3698e5aed4004c3
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-10-24 18:41:59 +0100

    q4.1 find learning rate complete

--- mlp/learning_rules.py
+++ mlp/learning_rules.py
@@ -338,11 +338,11 @@ class AdamLearningRuleWithWeightDecay(GradientDescentLearningRule):
 
         for param, mom_1, mom_2, grad in zip(
                 self.params, self.moms_1, self.moms_2, grads_wrt_params):
-            mom_1 = self.beta_1*mom_1 + (1-self.beta_1)*grad
-            mom_2 = self.beta_2*mom_2 + (1-self.beta_2)*grad**2
+            mom_1 = self.beta_1*mom_1 + (1-self.beta_1**self.step_count)*grad
+            mom_2 = self.beta_2*mom_2 + (1-self.beta_2**self.step_count)*grad**2
             # TODO: add the (self.learning_rate / self.initial_learning_rate)*
-            param -= (self.initial_learning_rate * mom_1 /
-                      ((mom_2 + self.epsilon)**0.5 + (self.weight_decay*param)))
+            param -= (self.learning_rate / self.initial_learning_rate)*(self.initial_learning_rate * mom_1 /
+                      ((mom_2 + self.epsilon)**0.5))  + (self.weight_decay*param)
 
 
 class AdaGradLearningRule(GradientDescentLearningRule):
