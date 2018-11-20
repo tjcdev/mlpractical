f106dd5bee9ea09b286047945fa8cc8a5d4556ca
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-20 22:36:12 +0000

    update names

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,11 @@
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 1
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type max_pooling --experiment_name filter_32_stride_1 --use_gpu True --stride 1
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 48 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 1
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 48 --dim_reduction_type max_pooling --experiment_name filter_48_stride_1 --use_gpu True --stride 1
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 48 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 2
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 48 --dim_reduction_type max_pooling --experiment_name filter_48_stride_2 --use_gpu True --stride 2
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 3
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_3 --use_gpu True --stride 3
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 96 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 4
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 96 --dim_reduction_type max_pooling --experiment_name filter_96_stride_4 --use_gpu True --stride 4
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 128 --dim_reduction_type max_pooling --experiment_name tutorial_exp_1 --use_gpu True --stride 5
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 128 --dim_reduction_type max_pooling --experiment_name filter_128_stride_5 --use_gpu True --stride 5
