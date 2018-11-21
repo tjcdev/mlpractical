ee165b8ea51780596a92471f67bd9461b1b83eec
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-21 21:04:23 +0000

    sh files

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,14 @@
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 48 --dim_reduction_type max_pooling --experiment_name filter_48_stride_2 --use_gpu True --stride 2
 
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_3 --use_gpu True --stride 3
 
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 96 --dim_reduction_type max_pooling --experiment_name filter_96_stride_4 --use_gpu True --stride 4
 
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 128 --dim_reduction_type max_pooling --experiment_name filter_128_stride_5 --use_gpu True --stride 5
+
+[DONE]
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type max_pooling --experiment_name filter_32_stride_2 --use_gpu True --stride 2
