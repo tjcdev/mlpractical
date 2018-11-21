1439d6be370bbfb4ad79da2f092e08373a22176e
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-21 21:43:01 +0000

    change exps

--- run_1.sh
+++ run_1.sh
@@ -1,3 +1,3 @@
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type max_pooling --experiment_name filter_32_stride_3 --use_gpu True --stride 3
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_1 --use_gpu True --stride 1
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type max_pooling --experiment_name filter_32_stride_4 --use_gpu True --stride 4
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_2 --use_gpu True --stride 2
