4c7ef007319550029459c9a384966fa5aeaefc95
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-22 21:19:05 +0000

    created dilation batch file

--- run_dil.sh
+++ run_dil.sh
@@ -1,1 +1,7 @@
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type dilated_convolution --experiment_name filter_32_dil_2 --use_gpu True --stride 1 --dilation 2
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type dilated_convolution --experiment_name filter_32_dil_2 --use_gpu True --stride 1 --dilation 3
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type dilated_convolution --experiment_name filter_32_dil_2 --use_gpu True --stride 1 --dilation 4
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type dilated_convolution --experiment_name filter_32_dil_2 --use_gpu True --stride 1 --dilation 5
