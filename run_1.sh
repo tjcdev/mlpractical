2cc9eec8f7e4d49037b21be13ebe8ab49a01bd80
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-23 17:05:29 +0000

    new experiment batch file

--- run_1.sh
+++ run_1.sh
@@ -1,3 +1,5 @@
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_1 --use_gpu True --stride 1
 
+[DONE]
 python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 64 --dim_reduction_type max_pooling --experiment_name filter_64_stride_2 --use_gpu True --stride 2
---
+++ run_stride.sh
@@ -0,0 +1,7 @@
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_1 --use_gpu True --stride 1 --dilation 0
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_2 --use_gpu True --stride 2 --dilation 0
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_4 --use_gpu True --stride 4 --dilation 0
+
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_8 --use_gpu True --stride 8 --dilation 0
