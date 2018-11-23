31e72433d6c003d582ee071091b5f74c3c856f0d
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-11-23 18:22:27 +0000

    change the epoch number

--- run_stride.sh
+++ run_stride.sh
@@ -1,7 +1,5 @@
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_1 --use_gpu True --stride 1 --dilation 1
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_2 --use_gpu True --stride 2 --dilation 1 --num_epochs 40
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_2 --use_gpu True --stride 2 --dilation 1
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_4 --use_gpu True --stride 4 --dilation 1 --num_epochs 40
 
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_4 --use_gpu True --stride 4 --dilation 1
-
-python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_8 --use_gpu True --stride 8 --dilation 1
+python mlp/pytorch_experiment_scripts/train_evaluate_emnist_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --dim_reduction_type strided_convolution --experiment_name new_filter_32_stride_8 --use_gpu True --stride 8 --dilation 1 --num_epochs 40
